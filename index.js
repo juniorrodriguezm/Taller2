aa53be790fcd102d188dff406b585e59adec29d8
Junior Rodriguez <junifer.21@hotmail.com>
2018-05-06 15:26:10 -0500

    Primer commit, lectura de hbs

---
+++ index.js
@@ -0,0 +1,29 @@
+const MongoClient = require('mongodb').MongoClient,
+express = require('express'),
+engines = require('consolidate');
+
+var app = express(), db;
+
+app.engine('hbs', engines.handlebars);
+
+app.set('views', './views');
+app.set('view engine', 'hbs');
+
+app.use(express.static('public'));
+
+// Conectarse a Base de Datos
+MongoClient.connect('mongodb://localhost:27017', function (err, client) {
+if (err) throw err;
+
+db = client.db('fuentes');
+
+// Iniciar servidor
+app.listen(5000);
+console.log("Escuchando servidor")
+});
+
+/*Esta parte es para cargar las paginas*/
+app.get('/', (req, res) => {
+    res.render('index', {
+    });
+})
